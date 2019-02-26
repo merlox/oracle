c8a8fb096444d29f26ac1787b9c64bc3eba71964
merlox <merunasgrincalaitis@gmail.com>
2019-02-26 14:49:19 +0100

    Testing git.sh

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@ if [ "$1" == "" ]; then
     echo "You must set a message when commiting"
     exit
 fi
-echo \"$1\"
-# git add .;
-git commit -m $1;
-# git push;
+
+git add .
+git commit -m "$1"
+git push
